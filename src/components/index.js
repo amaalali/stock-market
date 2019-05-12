c5f25942659a416191f85078806ede62ab6e34da
Amaal Ali <9220299+amaalali@users.noreply.github.com>
2019-05-12 23:11:35 +0100

    Restructure folder structure. t:w

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <>
+      {/* <header className="app-header">header</header> */}
+      <body>Body</body>
+      {/* <footer className="app-footer">footer</footer> */}
+    </>
+  );
+}
+
+export default App;
