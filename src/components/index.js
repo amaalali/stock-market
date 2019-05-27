f3052a5e5d23edffb811188429a7afe16631301c
Amaal Ali <9220299+amaalali@users.noreply.github.com>
2019-05-27 13:30:36 +0100

    Add form to page and add tailwind dependency.

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import "./index.css";
+
+const NewTrade = props => {
+  return (
+    <div className="foo">
+      <form className="">
+        <label htmlFor="stock-symbol">Symbol</label>
+        <input type="text" name="stock-symbol" id="stock-symbol" />
+
+        <label htmlFor="price">Price</label>
+        <input type="text" name="price" id="price" />
+
+        <label htmlFor="number-of-shares">Number of shares</label>
+        <input type="text" name="number-of-shares" id="number-of-shares" />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <>
-      {/* <header className="app-header">header</header> */}
-      <body>Body</body>
-      {/* <footer className="app-footer">footer</footer> */}
-    </>
+    <main className="app-main">
+      <NewTrade />
+    </main>
   );
 }
 
