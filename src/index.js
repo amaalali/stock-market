80b74cc42fdfca3859dda80310b76870bbae181e
Amaal Ali <9220299+amaalali@users.noreply.github.com>
2019-05-27 14:17:08 +0100

    Add TailwindCSS and PostCSS config.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // TODO: Change unregister() to register() to enable installation and offline work.
 // TL;DR on service workers: https://bit.ly/CRA-PWA
